e79652458a3fb0f650d0a25fdc7098b755cf5646
gilcu3 <gilcu3@gmail.com>
2025-11-22 18:58:11 +0100

    feat: improve args parsing

--- discretelog/__main__.py
+++ discretelog/__main__.py
@@ -1,11 +1,21 @@
-#! /usr/bin/env python3 -u
+#!/usr/bin/env python3
 
-import sys
+import argparse
 
 from . import dlog
 
-if __name__ == "__main__":
-    assert len(sys.argv) == 4, "Expecting parameters g (base), v (value), n (modulo)"
-    g, v, n = tuple(map(int, sys.argv[1:]))
-    d, dt = dlog(g, v, n)
+
+def main():
+    parser = argparse.ArgumentParser(prog="discretelog", description="Calculate discrete logarithm.")
+    parser.add_argument('-g', type=int, help="Logarithm base")
+    parser.add_argument("-n", type=int, help="Modulo")
+    parser.add_argument("v", type=int, help="Element")
+
+    args = parser.parse_args()
+
+    d, _dt = dlog(args.g, args.v, args.n)
     print(d)
+
+
+if __name__ == "__main__":
+    main()
