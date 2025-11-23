1c07ac9edc7d923c894843ad63cedf4ed261df3a
gilcu3 <gilcu3@gmail.com>
2025-11-23 13:39:53 +0100

    feat: improve args, add verbose

--- discretelog/__main__.py
+++ discretelog/__main__.py
@@ -9,13 +9,17 @@ def main() -> None:
     parser = argparse.ArgumentParser(
         prog="discretelog", description="Calculate discrete logarithm."
     )
-    parser.add_argument("-g", type=int, help="Logarithm base")
-    parser.add_argument("-n", type=int, help="Modulo")
-    parser.add_argument("v", type=int, help="Element")
+
+    parser.add_argument(
+        "-v", "--verbose", action="store_true", default=False, help="Verbose mode"
+    )
+    parser.add_argument("-g", "--base", type=int, help="Logarithm base")
+    parser.add_argument("-n", "--modulo", type=int, help="Modulo")
+    parser.add_argument("element", type=int, help="Element")
 
     args = parser.parse_args()
 
-    d, _dt = dlog(args.g, args.v, args.n)
+    d, _dt = dlog(args.base, args.element, args.modulo, args.verbose)
     print(d)
 
 
